2ceb9efbc91c19fbed81665b4249771f2c17d51d
fabianagsilva <bisilvag2@gmail.com>
2022-10-05 17:13:30 -0300

    @fabianagsilva [Inclusão do Projeto Integrador / DER / Dicionário] 05.10.2022

---
+++ ods11/src/test/java/com/generation/ods11/Ods11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.ods11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ods11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
